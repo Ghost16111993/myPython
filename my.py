757938562ec366997c623fd844dadbf2052813f4
Ghost16111993 <blinovaak@list.ru>
2025-04-20 12:54:49 +0300

    add my.py

--- my.py
+++ my.py
@@ -1,3 +1,15 @@
 print ("Nastu")
 
 print ("вторая строка")
+print("привет","мир" "мир")
+print(99, 88, 55.5 True, False)
+
+a = 6
+b = 5
+print(a + b)
+print("6 + 5")
+print("4","5", "6" ,sep="-")
+а = 7
+print(a)
+a +=1
+print (a)
