dc76b30c3714b319b79db15cd1bdbc6b97d5a991
Ghost16111993 <blinovaak@list.ru>
2025-05-09 17:42:33 +0300

    add my.py

---
+++ homework 27.04.py
@@ -0,0 +1,11 @@
+a = input("Как вас зовут? ")
+b = input('Сколько вам лет? ')
+c = input("Любите вы программировать? ")
+
+# словари
+user_info = {"name": a, "age": b, "like programming": c}
+print(user_info.values())
+print(user_info)
+
+
+
