b84f46ea7a270b331613dd3707351231033275b1
Ghost16111993 <blinovaak@list.ru>
2025-05-09 14:27:45 +0300

    add my.py

--- input 26.04.py
+++ input 26.04.py
@@ -4,6 +4,9 @@ c = input("Любите ли вы программировать? ")
 print("a")
 print("b")
 print("c")
+x = int(input("Введите значение x = "))
+y = int(input("Введите значение у = "))
+print(y > x - 2)
 
 
 
